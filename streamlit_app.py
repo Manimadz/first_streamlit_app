a8ba6629f13844ccf9db2b28fd4e53e5b3a3f538
Manimadz <123064154+Manimadz@users.noreply.github.com>
2023-01-19 01:08:52 -0800

    Update streamlit_app.py
    Updated text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfst Menu')
+streamlit.text('Omega 3 & Bluberry oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
