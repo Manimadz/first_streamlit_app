8ec17f778ce3fea1dd73a75bbbc707509e5d7840
Manimadz <123064154+Manimadz@users.noreply.github.com>
2023-01-19 01:15:55 -0800

    Update streamlit_app.py
    added emojies and changed header name

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfst Menu')
-streamlit.text('Omega 3 & Bluberry oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.header('Breakfst Favorites')
+streamlit.text('🥣Omega 3 & Bluberry oatmeal')
+streamlit.text('🥗kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Tost')
